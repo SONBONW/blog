6d975967f06682fbe10247bbac0457b87cf8d703
SonBon <120262749+SONBONW@users.noreply.github.com>
2023-07-28 15:58:07 +0700

    updateHtmlCss

--- asset/script/click.js
+++ asset/script/click.js
@@ -1,5 +1,38 @@
-const click = document.getElementById('click');
+ function click(nameclass, initial, replace) {
+     const element = document.getElementsByClassName(nameclass)[0];
+     const isNavbarOpen = element.classList.contains(replace);
+     if (isNavbarOpen) {
+         element.classList.remove(replace);
+         element.classList.add(initial);
+     } else {
+         element.classList.add(replace);
+         element.classList.remove(initial);
+     }
+ }
 
-click.addEventListener('click', () => {
-    
-})
+
+ function clickIconNav() {
+    //  const element = document.getElementsByClassName("item-menu")[0];
+    //  const isNavbarOpen = element.classList.contains("displayFlex");
+    //  if (isNavbarOpen) {
+    //      element.classList.remove("displayFlex");
+    //      element.classList.add("displayNone");
+    //  } else {
+    //      element.classList.add("displayFlex");
+    //      element.classList.remove("displayNone");
+     //  }
+     click("item-menu", "displayNone", "displayFlex");
+ }
+
+ function showSearch() {
+    //  const search = document.getElementsByClassName("search")[0]
+    //  const checkSearch = search.classList.contains("playFlex");
+    //  if (checkSearch) {
+    //      element.classList.remove("playFlex");
+    //      element.classList.add("playNone");
+    //  } else {
+    //      element.classList.add("playFlex");
+    //      element.classList.remove("playNone");
+     //  }
+     click("search", "playNone", "playFlex");
+ }
