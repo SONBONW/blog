62644edfa2f0727baf028ac5a3d97c32362dfcce
Nhech <apple@Nhech-MacBook-Pro.local>
2023-08-14 14:07:11 +0700

    updateHtmlCssBootstrap

--- asset/script/function.js
+++ asset/script/function.js
@@ -1,14 +1,3 @@
- function click(nameclass, position, initial, replace) {
-     const element = document.getElementsByClassName(nameclass)[position];
-     const check = element.classList.contains(replace);
-     if (check) {
-         element.classList.remove(replace);
-         element.classList.add(initial);
-     } else {
-         element.classList.add(replace);
-         element.classList.remove(initial);
-     }
- }
 
 
  /*Click Change Color Background*/
@@ -29,13 +18,13 @@ changeColor.addEventListener('click', () => {
 
 /*Get Value In Input*/
 
-let Title = document.getElementById("title");
-let Content = document.getElementById("content");
-let Img = document.getElementById("post-img")
-let submit = document.getElementById("submit");
+// let Title = document.getElementById("title");
+// let Content = document.getElementById("content");
+let img = document.getElementById("post-img");
+// let submit = document.getElementById("submit");
 
 
-function Check(val) {
+function checkValue(val) {
     if (val.value === "") {
         return false;
     }
@@ -43,41 +32,41 @@ function Check(val) {
 }
 
 function showError(val, error) {
-    if (!Check(val)) {
+    if (!checkValue(val)) {
         error.innerHTML = "Input has not content";
     } else {
         error.innerHTML = "";
     }
 }
 
-function eventChange(e, error) {
+function eventChangeError(e, error) {
     e.addEventListener("change", () => {
         error.innerHTML = "";
     })
 }
 
-eventChange(Title, errortitle);
-eventChange(Img, errorimg);
-eventChange(Content, errorcontent);
+eventChangeError(title, errortitle);
+eventChangeError(Img, errorimg);
+eventChangeError(content, errorcontent);
 
 submit.addEventListener("click", () => {
-    if (Check(Title) && Check(Img) && Check(Content)) {
+    if (checkValue(title) && checkValue(Img) && checkValue(content)) {
         alert("Submit");
     } else {
-        showError(Title, errortitle);
+        showError(title, errortitle);
         showError(Img, errorimg);
-        showError(Content, errorcontent);
+        showError(content, errorcontent);
     }
 });
 
 /*Limit number of characters in input title*/
 
 function inputCharacters() {
-  let numCharacters = Title.value.length;
+  let numCharacters = title.value.length;
 
   if (numCharacters >= 70) {
-    Title.value = Title.value.slice(0, 70);
-    Title.blur(); // Loại bỏ focus khỏi textarea để dừng lại nhận ký tự từ bàn phím
+    title.value = title.value.slice(0, 70);
+    title.blur(); // Loại bỏ focus khỏi textarea để dừng lại nhận ký tự từ bàn phím
   }
 }
 
@@ -85,12 +74,12 @@ function inputCharacters() {
 function countCharacters() {
   let charCount = document.getElementById("charCount");
 
-  let numCharacters = Content.value.length;
+  let numCharacters = content.value.length;
 
   charCount.textContent = numCharacters + " /10000";
 
   if (numCharacters >= 10000) {
-    Content.value = Content.value.slice(0, 10000);
-    Content.blur(); // Loại bỏ focus khỏi textarea để dừng lại nhận ký tự từ bàn phím
+    content.value = content.value.slice(0, 10000);
+    content.blur(); // Loại bỏ focus khỏi textarea để dừng lại nhận ký tự từ bàn phím
   }
 }
