d54e20e008f0b00b3d3cab83fd7a839871b8ac81
Nhech <apple@Nhech-MacBook-Pro.local>
2023-08-22 11:33:51 +0700

    updateHtmlCssBootstrap

---
+++ asset/script/adddatapost.js
@@ -0,0 +1,44 @@
+/*Add Post New Infor From Form */
+
+const testPost = [{
+    tag: "Technology",
+    title: "The Impact of Technology on the Workplace: How Technology is Changing",
+    time: "August 20, 2022",
+    user: {
+        avatar: "./asset/img/img-user2.png",
+        username: "Tracey Wilson",
+    }
+}];
+
+function addPost() {
+    const newPost = {
+        title: getValueInput(title),
+        tag: 'Technology',
+        time: formatDate(currentDate),
+        user: {
+            username: 'This is User Name',
+            avatar: 'This is Avater',
+        },
+    };
+    testPost.push(newPost);
+    console.log('New post added:', newPost);
+
+    /*Reset Input*/
+    title.value = '';
+    // tagInput.value = '';
+    // timeInput.value = '';
+    // usernameInput.value = '';
+}
+
+/*Get Time Now*/
+function formatDate(dateString) {
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+    return new Date(dateString).toLocaleDateString('en-US', options);
+}
+
+const currentDate = new Date(); // Lấy thời gian hiện tại
+// const formattedDate = formatDate(currentDate);
--- asset/script/rendexAjax.js
+++ asset/script/rendexAjax.js
@@ -53,8 +53,10 @@
 
 let posts = document.getElementsByClassName('posts')[0];
 let viewBtn = document.getElementsByClassName("view")[0];
-const NUMBER_REQUESTS = 9;
+let xhr = new XMLHttpRequest();
+const LIMIT_REQUESTS = 3;
 let current = 1;
+let total;
 
 let createPost = (post) => {
     let cardPost = document.createElement('div');
@@ -78,17 +80,44 @@ let createPost = (post) => {
 };
 
 let addPost = (data) => {
-    let startRange = (current - 1) * NUMBER_REQUESTS;
-    let endRange = startRange + NUMBER_REQUESTS;
+    let startRange = (current - 1) * LIMIT_REQUESTS;
+    let endRange = startRange + LIMIT_REQUESTS;
     for (let i = startRange; i < Math.min(endRange, data.length); i++) {
         createPost(data[i]);
     }
 };
 
-let loadData = () => {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', "http://localhost:3000/posts", true);
+
+
+function getData(url) {
+    xhr.open('GET', url, true);
     xhr.send();
+}
+
+/*Lấy độ dài của dữ liệu chính*/
+function pageSize() {
+    const url = "http://localhost:3000/posts";
+    getData(url);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            let data = JSON.parse(xhr.responseText);
+            total = data.length;
+            /*Sau khi lấy được độ dài của dữ liệu chính thì render dữ liệu theo mong muốn*/
+            loadData();
+        }
+
+    };
+}
+
+let displayedPosts = []; // Mảng lưu trữ các bài post đã hiển thị
+
+function loadData() {
+    const startRange = (current - 1) * LIMIT_REQUESTS;
+    const endRange = startRange + LIMIT_REQUESTS;
+    const url = `http://localhost:3000/posts?_start=${startRange}&_end=${endRange}`;
+    // const url = `http://localhost:3000/posts?_page=0&_limit=${LIMIT_REQUESTS * current}`;
+    getData(url);
+
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
             if (xhr.status != 200) {
@@ -96,20 +125,42 @@ let loadData = () => {
                 console.log(xhr.status);
             } else {
                 let data = JSON.parse(xhr.responseText);
-                /*Kiểm tra số lượng hiện ra đã hiển thị hết trong data chưa. Hết thì ẩn nút View*/
-                if (current * NUMBER_REQUESTS >= data.length) {
-                    viewBtn.classList.add('d-none');
-                }
-                addPost(data);
+                UpdateViewBtn();
+                console.log(data);
+                 // Nối thêm dữ liệu mới vào mảng displayedPosts
+                displayedPosts = [...data]; 
+
+                // Render lại toàn bộ danh sách
+                renderPosts(displayedPosts);
                 current++;
             }
         }
     };
+
 };
 
+function renderPosts(data) {
+    /*Render ra các bài viết trong displayedPosts*/
+    for (let i = 0; i < data.length; i++) {
+        createPost(data[i]);
+    }
+}
+
+/*Kiểm tra số lượng hiện ra đã hiển thị hết trong data chưa. Hết thì ẩn nút View*/
+function UpdateViewBtn() {
+    if (current * LIMIT_REQUESTS >= total) {
+        viewBtn.classList.add('d-none');
+    } else {
+        viewBtn.classList.remove('d-none');
+    }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+    // console.log(total);
     viewBtn.addEventListener("click", () => {
         loadData();
+
     });
-    loadData();
+    /*Render dữ liệu ra màn hình*/
+    pageSize();
 });
